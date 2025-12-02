7d6daa64be9ccbeaf6579f4a7e10f6abd65bec55
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-02 19:02:41 +0530

    Add files via upload

---
+++ Python Basics/Chapter 2/1 variables.py
@@ -0,0 +1,7 @@
+a = 1   #variable a is assigned the value 1
+
+b = 3  #variable b is assigned the value 3
+
+c = a + b
+
+print("Sum of a and b is :",c)
---
+++ Python Basics/Chapter 2/2 datatypes.py
@@ -0,0 +1,5 @@
+a = 1       # a is an integer
+b = 3.9     # b is a float point number 
+c = "Viraj" # c is a string
+d = True    # d is a boolean variable
+E = None    # E is a NoneType variable (Nothing is there not True or False)
