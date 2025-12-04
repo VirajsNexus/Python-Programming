2d774bbe008827fd6f3dd1580cdb66c48c64b290
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 14:30:26 +0530

    Create 1 store Fruits in a list.py

---
+++ Python Basics/Chapter 4 Practice Set/1 store Fruits in a list.py
@@ -0,0 +1,25 @@
+Fruits = []  #Empty list created to store fruits
+
+f1 = input("Enter the name of fruit 1: ")  #Input for fruit 1
+Fruits.append(f1)  #Adding fruit 1 to the list
+
+f2 = input("Enter the name of fruit 2: ")  #Input for fruit 1
+Fruits.append(f2)  #Adding fruit 2 to the list
+
+f3 = input("Enter the name of fruit 3: ")  #Input for fruit 1
+Fruits.append(f3)  #Adding fruit 3 to the list
+
+f4 = input("Enter the name of fruit 4: ")  #Input for fruit 1
+Fruits.append(f4)  #Adding fruit 4 to the list
+
+f5 = input("Enter the name of fruit 5: ")  #Input for fruit 1
+Fruits.append(f5)  #Adding fruit 5 to the list
+
+f6 = input("Enter the name of fruit 6: ")  #Input for fruit 1
+Fruits.append(f6)  #Adding fruit 6 to the list
+
+f7 = input("Enter the name of fruit 7: ")  #Input for fruit 1
+Fruits.append(f7)  #Adding fruit 7 to the list
+
+print("\n\nThe fruits you entered are: ")  #Printing message
+print(Fruits)  #Printing the list of fruits
