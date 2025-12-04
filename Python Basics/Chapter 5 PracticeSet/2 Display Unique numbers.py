1c06e87ede813d9c0b2041ee229a443d267331a7
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 23:20:48 +0530

    Add files via upload

---
+++ Python Basics/Chapter 5 PracticeSet/2 Display Unique numbers.py
@@ -0,0 +1,27 @@
+numbers = set() #Empty set to store unique numbers
+empty_set = numbers
+
+num = int(input("Enter the number 1: "))
+numbers.add(num)
+
+num = int(input("Enter the number 2: "))
+numbers.add(num)
+
+num = int(input("Enter the number 3: "))
+numbers.add(num)
+
+num = int(input("Enter the number 4: "))
+numbers.add(num)
+
+num = int(input("Enter the number 5: "))
+numbers.add(num)
+
+num = int(input("Enter the number 6: "))
+numbers.add(num)
+
+num = int(input("Enter the number 7: "))
+numbers.add(num)
+
+print("The unique numbers you entered are: ", set)
+set = empty_set # Assigning the set to a variable 'set' for printing
+print("The unique numbers you entered are: ", set)
