9289f42dd91a5985c682b26172db0448f59a892a
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 23:51:05 +0530

    Create 1 for loop.py
    Added explanation and example of for loop in Python.

---
+++ Python Basics/Chapter 7/1 for loop.py
@@ -0,0 +1,15 @@
+print("\nWhat is a for loop?\n " \
+"\nA for loop is a control flow statement that allows code to be executed repeatedly based on a sequence or range. " \
+"It is commonly used for iterating over a collection of items, such as a list or a range of numbers.\n")
+print("\nPrinting numbers from 1 to 5 using individual print statements:")
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+print("\nPrinting numbers from 1 to 5 using a for loop:")
+# The same task can be done like this:
+for i in range(1, 6):
+    print(i)
