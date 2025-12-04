efd71568d52109d62be28a3d9ac4acd4e1fcb9cc
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 13:40:06 +0530

    Add files via upload

---
+++ Python Basics/Chapter 2 Practice Set/2 Remainder of  two numbers.py
@@ -0,0 +1,4 @@
+a = 42
+b =  4
+
+print("Remainder of a and b is : ",a % b)
---
+++ Python Basics/Chapter 2 Practice Set/6 Square of num.py
@@ -0,0 +1,6 @@
+num = int(input("Enter your num : "))
+
+print("Square of Number you Entered is : ", num ** 2)
+print("Square of Number you Entered is : ", num * num)
+
+# print("Square of Number you Entered is : ",num^2 )  #invalid
