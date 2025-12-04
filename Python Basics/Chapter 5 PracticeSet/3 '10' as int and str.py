3cddbfde25834298f77a3703a5d1a3612feb8cad
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 23:21:33 +0530

    Delete Python Basics/Chapter 5 PracticeSet directory

--- Python Basics/Chapter 5 PracticeSet/3 '10' as int and str.py
+++
@@ -1,6 +0,0 @@
-set1 = set()
-set1.add(10)
-set1.add("10")
-
-print("The Set with '10' as astring and int is : ", set1)  # Output: {10, '10'}
-# This code demonstrates that the integer 18 and the string '18' are treated as distinct elements in a set.
--- Python Basics/Chapter 5 PracticeSet/4 Find length of set.py
+++
@@ -1,9 +0,0 @@
-set1 = set()
-set1.add(20)
-set1.add(20.0)
-set1.add('20') 
-
-
-print("The Set is : ", set1)
-
-print("The length of 'set1' is : ",len(set1))       #Expected Output : 2
