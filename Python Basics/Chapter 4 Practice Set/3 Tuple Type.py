49ea6028e125d38792277b03036d794bf543ecf2
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 19:08:35 +0530

    Create 3 Tuple Type.py

---
+++ Python Basics/Chapter 4 Practice Set/3 Tuple Type.py
@@ -0,0 +1,9 @@
+tuple1 = (21, 26, "Viraj", 32.4)
+
+print("Type of 'tuple1' is: ", type(tuple1))
+print("The original tuple is: ", tuple1)
+
+# tuple1[3] = "Jamdhade"      #Error: 'tuple' object does not support item assignment
+
+# print("The modified tuple is: ", tuple1)
+print("Tuples are immutable, so we cannot change their elements after creation.")
