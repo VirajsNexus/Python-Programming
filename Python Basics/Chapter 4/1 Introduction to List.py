f249a046c19a3880e0527279cb7a430221f79559
Viraj Jamdhade <virajjamdhade6@gmail.com>
2025-12-04 14:22:44 +0530

    Fix typos and enhance print output in list introduction
    Corrected typos and improved print statements for clarity.

--- Python Basics/Chapter 4/1 Introduction to List.py
+++ Python Basics/Chapter 4/1 Introduction to List.py
@@ -1,5 +1,12 @@
-print("Introduction to Lists in pytthon : ")
+print("\nUnlick Strings, Lists are mutable i.e. \nwe can change their content(Stings can be replaces in list)\n\n")
+
+print("Introduction to Lists in python : ")
 my_list = [1, 2, 3, "Viraj", 5.6, True]    # in list we can store multiple data types
 
-print("type of My List : ", type(my_list))
-print("My List : ", my_list)
+print("\ntype of My List : ", type(my_list))
+print("\nMy List : ", my_list)
+
+print("\nElement at position 4 is : ",my_list[3])    # accessing element at index 3
+
+# Slicing in Lists
+print("\nSlicing in Lists : ",my_list[1:4])    # accessing elements from index 1 to 3
